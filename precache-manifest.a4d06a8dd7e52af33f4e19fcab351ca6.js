48f48e0a1383a3a20932e79b205642566da8c24a
Edward Adam Farris <edwardadamfarris@Edwards-MacBook-Pro.local>
2019-03-30 13:15:53 -0700

    Updates

---
+++ precache-manifest.a4d06a8dd7e52af33f4e19fcab351ca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "5f4ce78a26b31ec6d3ee",
+    "url": "/clickygame/static/js/main.df693628.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/clickygame/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5f4ce78a26b31ec6d3ee",
+    "url": "/clickygame/static/css/main.1648221b.chunk.css"
+  },
+  {
+    "revision": "65a7daf7ae147dc6ef730c21b960bb1b",
+    "url": "/clickygame/index.html"
+  }
+];
